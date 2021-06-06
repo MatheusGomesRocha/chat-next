dfec209e0bdbc99dc374acf3a662076b74cb59f6
Matheus Gomes <matheusgomes192@hotmail.com>
2021-06-06 20:48:11 -0300

    Criando file _document para importar font MontSerrat + fazendo Header do Chats Component

--- src/components/Chats/index.tsx
+++ src/components/Chats/index.tsx
@@ -1,10 +1,17 @@
 import styles from './styles.module.scss';
+import { BiMenuAltLeft } from 'react-icons/bi';
 
 export default function Chats() {
     return(
         <div className={styles.container}>
             <div className={styles.header}>
-                
+                <BiMenuAltLeft color="#000" size={30} />
+
+                <h2>Messages</h2>
+
+                <div className={styles.messageNotRead}>
+                    <span>6</span>
+                </div>
             </div>
         </div>
     )
