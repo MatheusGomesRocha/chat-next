dcefacdfc64b3e9a8f5238a20d7a10f0b1f966db
Matheus Gomes <matheusgomes192@hotmail.com>
2021-06-08 14:00:49 -0300

    Fianlizando area Filter, onde o usuário filtraria sua pesquisa para procurar ligações, conversas e etc

--- src/components/Chats/index.tsx
+++ src/components/Chats/index.tsx
@@ -1,5 +1,8 @@
 import styles from './styles.module.scss';
 import { BiMenuAltLeft, BiSearchAlt2 } from 'react-icons/bi';
+import { RiMessage2Fill } from 'react-icons/ri';
+import { FiPhone, FiFileText } from 'react-icons/fi';
+import { TiContacts } from 'react-icons/ti';
 
 export default function Chats() {
     return(
@@ -17,6 +20,28 @@ export default function Chats() {
             <div className={styles.searchArea}>
                 <BiSearchAlt2 color="#B2B2B2" size={20} />
                 <input placeholder="Search pepole, groups & messages" />
+            </div>
+
+            <div className={styles.filterArea}>
+                <div>
+                    <RiMessage2Fill color="#3b00ff" size={25} />
+                    <span style={{color: '#3b00ff'}}>All Messages</span>
+                </div>
+                
+                <div>
+                    <FiPhone color="#9c9c9c" size={25} />
+                    <span>Calls</span>
+                </div>
+
+                <div>
+                    <TiContacts color="#9c9c9c" size={25} />
+                    <span>Contacts</span>
+                </div>
+
+                <div>
+                    <FiFileText color="#9c9c9c" size={25} />
+                    <span>Archived</span>
+                </div>
             </div>
         </div>
     )
