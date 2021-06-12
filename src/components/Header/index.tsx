b40b307aee086d8fbfdf00bf2cbc0ffc32061ec7
Matheus Gomes <matheusgomes192@hotmail.com>
2021-06-12 11:34:55 -0300

    Finalizando Header + começando a estilizar a tela principal (onde aparece as mensagens)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,9 @@
 import styles from './styles.module.scss';
 
+import { FiPhone } from 'react-icons/fi';
+import { BsCameraVideo } from 'react-icons/bs';
+import { IoEllipsisVerticalSharp } from 'react-icons/io5';
+
 export default function Header() {
     return(
         <div className={styles.container}>
@@ -13,6 +17,20 @@ export default function Header() {
                         <div className={styles.onlineIndicator} />
                         <span>Active Now</span>
                     </div>
+                </div>
+            </div>
+
+            <div className={styles.right}>
+                <div>
+                    <FiPhone color="#9c9c9c" size={25} />
+                </div>
+
+                <div>
+                    <BsCameraVideo color="#9c9c9c" size={25} />
+                </div>
+
+                <div>
+                    <IoEllipsisVerticalSharp color="#9c9c9c" size={25} />
                 </div>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import styles from './home.module.scss';
+
 export default function Home() {
   return(
-    <div>
+    <div className={styles.div}>
       <h1>Olá mundo</h1>
     </div>
   )
