aa2be0d1dc8f1904a4cfa60d4750c392846172c5
Matheus Gomes <matheusgomes192@hotmail.com>
2021-06-12 11:18:59 -0300

    Criando left area do Header (onde fica foto do perfil, nome do usuário e se ele está online)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,18 @@ import styles from './styles.module.scss';
 export default function Header() {
     return(
         <div className={styles.container}>
-        
+            <div className={styles.left}>
+                <img src="/user1.png" />
+
+                <div className={styles.leftColumn}>
+                    <span className={styles.userName}>Gabriel Aids</span>
+
+                    <div className={styles.isOnline}>
+                        <div className={styles.onlineIndicator} />
+                        <span>Active Now</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
